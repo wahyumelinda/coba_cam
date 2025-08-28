94492e76d107b27491141911efd8efde84bdee7e
wahyu melinda <wahyumelindaa@gmail.com>
2025-08-28 22:32:03 +0700

    Update coba_cam.py

--- coba_cam.py
+++ coba_cam.py
@@ -1,20 +1,10 @@
-import av
-import cv2
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+from PIL import Image
 
-st.title("Live Kamera dengan OpenCV + Streamlit")
+st.title("Ambil Foto di Streamlit Cloud")
 
-# Bikin class untuk transformasi video frame
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
+img_file = st.camera_input("Ambil gambar")
 
-        # Contoh: ubah jadi grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
-
-        return gray
-
-# Jalankan webrtc streamer
-webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+if img_file is not None:
+    img = Image.open(img_file)
+    st.image(img, caption="Hasil Foto")
